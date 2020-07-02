f3ce56786fe13b0a3a48f2fc0bf5b2d879a42fc0
ghh2013 <ghh2004@naver.com>
2020-07-02 17:44:10 +0900

    0702

--- RPG/Assets/GHH/Scripts/PalyerMove.cs
+++ RPG/Assets/GHH/Scripts/PalyerMove.cs
@@ -13,9 +13,8 @@ public class PalyerMove : MonoBehaviour
     float jumpPower = 10;
     int jumpcount = 0;
 
-    
+    float h, v;
 
-    
     Animator anim;
 
     int hp = 100;
@@ -23,11 +22,17 @@ public class PalyerMove : MonoBehaviour
     
     float attTime = 2f;
     float timer = 0f;
+
+    bool move = false;
+    bool attack = false;
+    bool die = false;
+    bool damaged = false;
+    bool grenade = false;
+    bool jump = false;
+    
     // Start is called before the first frame update
     void Start()
-    {
-        
-       
+    {       
         cc = GetComponent<CharacterController>();
         anim = GetComponentInChildren<Animator>();
     }
@@ -35,13 +40,100 @@ public class PalyerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (v != 0)
+        {
+            move = true;
+        }
+        else
+        {
+            move = false;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            attack = true;
+        }
+        else
+        {
+            attack = false;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            grenade = true;
+        }
+        else
+        {
+            grenade = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+        AnimationUpdate();
         Move();
     }
-    
+
+
+    private void AnimationUpdate()
+    {
+        if (move == true)
+        {
+            anim.SetBool("move", true);
+        }
+        else
+        {
+            anim.SetBool("move", false);
+        }
+        if (attack == true)
+        {
+            anim.SetBool("attack", true);
+        }
+        else
+        {
+            anim.SetBool("attack", false);
+        }
+        if (damaged == true)
+        {
+            anim.SetBool("damaged", true);
+        }
+        else
+        {
+            anim.SetBool("damaged", false);
+        }
+        if (die == true)
+        {
+            anim.SetBool("die", true);
+        }
+        else
+        {
+            anim.SetBool("die", false);
+        }
+        if (grenade == true)
+        {
+            anim.SetBool("grenade", true);
+        }
+        else
+        {
+            anim.SetBool("grenade", false);
+        }
+        if (jump == true)
+        {
+            anim.SetBool("jump", true);
+        }
+        else
+        {
+            anim.SetBool("jump", false);
+        }
+    }
+
     private void Move()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
         Vector3 dir = new Vector3(h, 0, v);
 
         dir = Camera.main.transform.TransformDirection(dir);
